a807ea44e5e107533eff5c642b52db44f029e27f
alisanhos <alexhossdorf@hotmail.com>
2025-06-16 17:41:53 +0200

    [FEAT] Listar campaña (Front) #19
    - fix: bajar la versión de React y React-DOM a la 18.2.0 y actualizar las definiciones de tipos correspondientes.
    - feat: agregar el componente Sidebar al diseño principal de la aplicación.
    - chore: actualizar la configuración de Tailwind CSS para incluir los componentes de Material Tailwind.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+
 import Footer from './components/Footer.jsx';
 import Navbar from './components/Navbar.jsx';
 import ListarCampanas from './components/campanas/ListarCampanas.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar.jsx';
 
 function App() {
 	return (
 		<div className='min-h-screen flex flex-col'>
 			<BrowserRouter>
 				<Navbar />
+				<Sidebar />
 				<main className='flex-grow container pt-20'>
 					<Routes>
 						<Route exact path='/' element={<ListarCampanas />} />
