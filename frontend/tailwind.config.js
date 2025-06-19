7868b90912a14ddadb668807d9774b1c686def45
alisanhos <alexhossdorf@hotmail.com>
2025-06-19 18:15:47 +0200

    [FEAT] #19: Actualizar dependencias
    - Se eliminaron dependencias no utilizadas del archivo package.json.
    
    - Se actualizaron las versiones de React y React-DOM a la 18.2.0.
    
    - Se cambió la versión de Tailwind CSS a la 3.4.17 y se ajustó su configuración.
    
    - Se refactorizó el componente Navbar para utilizar componentes de Material Tailwind.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,29 +1,15 @@
-export default {
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    // Añade esta línea si usas archivos de Material Tailwind
+    "node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-        color: {
-          primary: '#1E40AF', // Blue
-          secondary: '#FBBF24', // Yellow
-          accent: '#EF4444', // Red
-          neutral: '#374151', // Gray
-          'base-100': '#FFFFFF', // White
-        },
-        container: {
-          center: true,
-          padding: {
-            default: '1rem',
-            sm: '2rem',
-            md: '3rem',
-            lg: '4rem',
-            xl: '5rem',
-            '2xl': '6rem',
-          },
-        },
-    },
+    extend: {},
   },
   plugins: [],
-}
+});
