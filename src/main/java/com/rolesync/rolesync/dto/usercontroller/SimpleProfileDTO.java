174a822c134e3d3a19a0b6cebeca90514013ed93
Samalbalat <saoalbalat@gmail.com>
2026-02-14 15:47:48 +0100

    Codacy review

--- src/main/java/com/rolesync/rolesync/dto/usercontroller/SimpleProfileDTO.java
+++ src/main/java/com/rolesync/rolesync/dto/usercontroller/SimpleProfileDTO.java
@@ -2,16 +2,15 @@ package com.rolesync.rolesync.dto.usercontroller;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SimpleProfileDTO {
 
     private Long id;
     private String profileName;
     private byte[] image;
-
-    public SimpleProfileDTO() {
-    }
     
 }
--- src/main/java/com/rolesync/rolesync/dto/usercontroller/UserInfoResponseDetailed.java
+++ src/main/java/com/rolesync/rolesync/dto/usercontroller/UserInfoResponseDetailed.java
@@ -3,15 +3,14 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserInfoResponseDetailed {
     Long id;
     String email;
     String timeZone;
     List<SimpleProfileDTO> profiles;
-
-    public UserInfoResponseDetailed() {
-    }
 }
