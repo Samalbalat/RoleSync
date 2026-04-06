7f290b44f9946dc567813cbfdb16876ace38bef3
Samalbalat <saoalbalat@gmail.com>
2026-04-06 16:39:55 +0200

    [FIX-B] Campaign y Character Fixes #61 Cambios menores

--- src/main/java/com/rolesync/rolesync/controller/CampaignController.java
+++ src/main/java/com/rolesync/rolesync/controller/CampaignController.java
@@ -155,7 +155,7 @@ public class CampaignController {
                             item.setImage(c.getImage());
                             item.setSystem(c.getSystem());
                             item.setStatus(c.getStatus().name());
-                            Integer pendingRequests = Integer.valueOf(campaignRequestRepository.countPendingRequestsByCampaignId(c.getId()));
+                            Integer pendingRequests = Integer.valueOf(campaignRequestRepository.countPendingRequestsByCampaignId(c.getId()).toString());
                             item.setPendingRequests(pendingRequests);
 
                             return item;
