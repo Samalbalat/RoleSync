7a96c18b8bf30c46293b79dce361cf7f625b0f38
Samalbalat <saoalbalat@gmail.com>
2026-04-17 18:43:14 +0200

    Eliminated unnecesary print

--- src/main/java/com/rolesync/rolesync/controller/ProfileController.java
+++ src/main/java/com/rolesync/rolesync/controller/ProfileController.java
@@ -67,7 +67,6 @@ public class ProfileController {
         {
         Optional<Profile> profile = profileRepository.findByProfilename(profileToGet);
         if (profile.isPresent()) {
-            System.out.println("Profile found: " + profile.get());
             ProfileInDTO response = profile.map(p -> 
             new ProfileInDTO(p.getProfilename(), p.getImage(), p.getDescription())
         ).orElse(null);
