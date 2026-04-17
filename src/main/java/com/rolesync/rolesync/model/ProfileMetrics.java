bc3be4a85f8fbb16b1847922851ecd897f34fe98
Samalbalat <saoalbalat@gmail.com>
2026-04-17 18:51:47 +0200

    ExcludeProfileFromToString

--- src/main/java/com/rolesync/rolesync/model/ProfileMetrics.java
+++ src/main/java/com/rolesync/rolesync/model/ProfileMetrics.java
@@ -10,10 +10,12 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.ToString;
 
 @Entity
 @Table(name = "profile_metrics")
 @Data
+@ToString(exclude = "profile")
 public class ProfileMetrics {
 
     @Id
