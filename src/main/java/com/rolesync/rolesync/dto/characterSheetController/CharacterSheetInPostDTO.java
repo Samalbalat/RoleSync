bd8119ca23230262b12dbba82083f57e5c2cb909
Samalbalat <saoalbalat@gmail.com>
2026-04-19 09:35:43 +0200

    Se arregla el detalle de personaje

--- src/main/java/com/rolesync/rolesync/dto/characterSheetController/CharacterSheetInPostDTO.java
+++ src/main/java/com/rolesync/rolesync/dto/characterSheetController/CharacterSheetInPostDTO.java
@@ -13,6 +13,7 @@ public class CharacterSheetInPostDTO {
     
     private String name;    
     private String avatar_url;
+    private Boolean isPublic;
     private Long campaign_id;
     private Long template_id;
     private List<CharacterSchemaField> attributes;
